4a5e912337f15317fee4d63d300dee092a8cfc5d
Jakob <siegerjakob@gmail.com>
2026-07-21 17:06:45 +0200

    split initialization of table into separate functions to re-use logic to fill the table for sorting and filterung

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,40 @@
-async function loadCompanyData() {
-    const tableBody = document.getElementById("companyDataTable");
-    const response = await fetch("/assets/data/dummy-unternehmen-100.json")
-        .then(async function (response) {
-            const companyData = await response.json();
-            companyData.forEach(company => {
-                const row = document.createElement("tr");
-                row.innerHTML = `<td>${company.name}</td>
-                <td>${company.land}</td>
-                <td>${company.branche}</td>
-                <td>${company.mitarbeiter}</td>
-                <td>${company.jahresumsatz}</td>
-                <td>${company.co2Emissionen}</td>`;
-                tableBody.appendChild(row);
-            });
-        }).catch(function (error) {
-            tableBody.innerHTML = `<tr><td>Fehler beim Laden der Daten</td></tr>`;
-            console.log(`error thrown when loading company data: ${error}`);
-        });
+let companyDataJson = [];
+initTable();
+
+async function initTable() {
+    companyDataJson = await loadDataFromJson();
+    fillTable(companyDataJson);
 }
 
-loadCompanyData();
+async function loadDataFromJson() {
+    try {
+        const response = await fetch("/assets/data/dummy-unternehmen-100.json");
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.log(`error thrown when loading company data: ${error}`);
+    }
+    return [];
+}
+
+function fillTable(jsonData) {
+    const tableBody = document.getElementById("companyDataTable");
+    tableBody.innerHTML = "";
+
+    if (jsonData.length == 0) {
+        ableBody.innerHTML = "tried to fill table with empty data"
+    }
+
+    jsonData.forEach(company => {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td>${company.name}</td>
+                <td>${company.land}</td>
+                <td>${company.branche}</td>
+                <td>${company.mitarbeiter.toLocaleString("de-DE")}</td>
+                <td>${company.jahresumsatz.toLocaleString("de-DE")} €</td>
+                <td>${company.co2Emissionen.toLocaleString("de-DE")} t/Jahr</td>`;
+        tableBody.appendChild(row);
+    });
+}
